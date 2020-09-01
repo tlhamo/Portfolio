4712331aa450989be44588d6405648bc22f34c71
tlhamo <tsekilhamo@gmail.com>
2020-09-01 14:49:54 -0500

    Updates

---
+++ precache-manifest.d47ea6898d87489d28b2f73873ad5cfd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9329ca5bf19cd1b20054ad4dabf98715",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e9d57464bb625c5dac7f",
+    "url": "/portfolio/static/css/2.a8f673c4.chunk.css"
+  },
+  {
+    "revision": "825694fc1c4759191c22",
+    "url": "/portfolio/static/css/main.a8efb3d0.chunk.css"
+  },
+  {
+    "revision": "e9d57464bb625c5dac7f",
+    "url": "/portfolio/static/js/2.41b14057.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.41b14057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825694fc1c4759191c22",
+    "url": "/portfolio/static/js/main.53ca7398.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "21252e57a4b3afabaed6164c947ec7f3",
+    "url": "/portfolio/static/media/Tseki-Lhamo-Resume.21252e57.pdf"
+  },
+  {
+    "revision": "cb2e676a519c4963f0ead113360d060a",
+    "url": "/portfolio/static/media/business-demo.cb2e676a.png"
+  },
+  {
+    "revision": "8a376cba89cc310b34e84ec6795256d9",
+    "url": "/portfolio/static/media/chess-demo.8a376cba.png"
+  },
+  {
+    "revision": "03430e0b315cc912a1b575e1eb348de6",
+    "url": "/portfolio/static/media/earphones-demo.03430e0b.png"
+  },
+  {
+    "revision": "4aa04f36dbe693e9fa9e6ad64d960599",
+    "url": "/portfolio/static/media/headphones-demo.4aa04f36.png"
+  },
+  {
+    "revision": "967818a2e9db66a0b7dfd00b886da6e3",
+    "url": "/portfolio/static/media/homepage-background.967818a2.png"
+  },
+  {
+    "revision": "28727a48bdef8750cc61b55b73790271",
+    "url": "/portfolio/static/media/profile-picture.28727a48.png"
+  },
+  {
+    "revision": "1495a0394c2e0714c9b8cde77944785f",
+    "url": "/portfolio/static/media/project-homepage.1495a039.png"
+  },
+  {
+    "revision": "3cd5f5b216688fee8dec6f0727b70796",
+    "url": "/portfolio/static/media/recipe-demo.3cd5f5b2.png"
+  }
+]);
